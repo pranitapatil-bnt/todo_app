ba27852b1534542939133f3a7a2c56a8177b36e0
Pranita Patil <pranita.patil@bnt-soft.com>
2024-01-24 23:41:39 +0530

    creating a to-do app list

---
+++ src/CreateTodo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CreateTodo() {
+  return (
+    <div className="create_form">
+        <input type='text' name='' id=''/>
+        <button type='button'>Add</button>
+    </div>
+  )
+}
